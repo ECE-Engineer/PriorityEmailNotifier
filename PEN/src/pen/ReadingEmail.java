cc44cfb417aaf8ef4e95213bb7b54f732f2d128a
Kyle <kzeller@oswego.edu>
2016-03-06 05:42:50 -0500

    SIXTH Commit

--- PEN/src/pen/ReadingEmail.java
+++ PEN/src/pen/ReadingEmail.java
@@ -26,14 +26,6 @@ public class ReadingEmail {
     //static String[] components;
     static String priority;
     //static ArrayList <String[]> userInfo = new ArrayList <String[]>();
-//    
-//    String d_email = "fromAddress@gmail.com",
-//            d_password = "password", //your email password
-//            d_host = "smtp.gmail.com",
-//            d_port = "465",
-//            m_to = "ToAddress", // Target email address
-//            m_subject = "Testing",
-//            m_text = "Hey, this is a test email.";
     
     public static void main(String[] args) {
         setMessageList();
@@ -122,40 +114,4 @@ public class ReadingEmail {
             mex.printStackTrace();
         }
     }
-    
-//    
-//    public ReadingEmail() {
-//        Properties props = new Properties();
-//        props.put("mail.smtp.user", d_email);
-//        props.put("mail.smtp.host", d_host);
-//        props.put("mail.smtp.port", d_port);
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.smtp.auth", "true");
-//        //props.put("mail.smtp.debug", "true");
-//        props.put("mail.smtp.socketFactory.port", d_port);
-//        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//        props.put("mail.smtp.socketFactory.fallback", "false");
-//        try {
-//            Authenticator auth = new SMTPAuthenticator();
-//            Session session = Session.getInstance(props, auth);     
-//            MimeMessage msg = new MimeMessage(session);
-//            msg.setText(m_text);
-//            msg.setSubject(m_subject);
-//            msg.setFrom(new InternetAddress(d_email));
-//            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(m_to));
-//            Transport.send(msg);
-//        } catch (Exception mex) {
-//            mex.printStackTrace();
-//        }
-//    }
-//   
-//    public static void main(String[] args) {
-//        ReadingEmail blah = new ReadingEmail();
-//    }
-//  
-//    private class SMTPAuthenticator extends javax.mail.Authenticator {
-//        public PasswordAuthentication getPasswordAuthentication() {
-//            return new PasswordAuthentication(d_email, d_password);
-//        }
-//    }
 }
